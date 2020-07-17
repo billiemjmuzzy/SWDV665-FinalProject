20de035e056b0d9df1ffa04491bbdd22431b00ca
Billie <billiemj@gmail.com>
2020-07-16 19:20:10 -0500

    scorecard-base

--- golf/src/app/tabs/tabs-routing.module.ts
+++ golf/src/app/tabs/tabs-routing.module.ts
@@ -16,8 +16,8 @@ const routes: Routes = [
         loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
       },
       {
-        path: 'tab3',
-        loadChildren: () => import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+        path: 'scorecard',
+        loadChildren: () => import('../scorecard/scorecard.module').then(m => m.ScorecardPageModule)
       },
       {
         path: '',
