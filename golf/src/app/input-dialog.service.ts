5cca5c6c720244d0ab70c99f0d500d2fac8daecd
Billie <billiemj@gmail.com>
2020-08-16 13:11:30 -0500

    updates

--- golf/src/app/input-dialog.service.ts
+++ golf/src/app/input-dialog.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AlertController } from '@ionic/angular';
-import { TournamentService } from './tournament.service.';
+import { TournamentService } from './tournament.service';
 
 @Injectable({
   providedIn: 'root'
