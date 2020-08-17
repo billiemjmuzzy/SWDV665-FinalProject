b695af7f9211366f591669ad1cc3c5f5c0db8aec
Billie <billiemj@gmail.com>
2020-08-17 12:12:29 -0500

    added auth

--- golf/src/app/app-routing.module.ts
+++ golf/src/app/app-routing.module.ts
@@ -2,13 +2,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+
+import { AuthGuard } from './auth/auth.guard';
+
 const routes: Routes = [
   { path: '', redirectTo: 'about', pathMatch: 'full' },
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthPageModule) },
-  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutPageModule) },
-  { path: 'rules', loadChildren: () => import('./Rules/Rules.module').then(m => m.RulesPageModule) },
-  { path: 'tournament', loadChildren: () => import('./tournament/tournament.module').then(m => m.TournamentPageModule) },
-  { path: 'scorecard', loadChildren: () => import('./scorecard/scorecard.module').then(m => m.ScorecardPageModule) }
+  {
+    path: 'about',
+    loadChildren: () => import('./about/about.module').then(m => m.AboutPageModule),
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'rules',
+    loadChildren: () => import('./rules/rules.module').then(m => m.RulesPageModule),
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'tournament',
+    loadChildren: () => import('./tournament/tournament.module').then(m => m.TournamentPageModule),
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'scorecard',
+    loadChildren: () => import('./scorecard/scorecard.module').then(m => m.ScorecardPageModule),
+    canLoad: [AuthGuard]
+  }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- golf/src/environments/environment.prod.ts
+++ golf/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  googleMapsAPIKey: 'KEY',
+  firebaseAPIKey: 'AIzaSyC1k63xdwVY_Z8GV0dzJesZqr8Tx1rdu4w'
 };
